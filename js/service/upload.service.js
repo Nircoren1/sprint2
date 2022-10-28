6e65ad4f2d8f87bcd21122ef72cd832bd4e24eab
Nir <nircoren@gmail.com>
2022-10-28 12:29:53 +0300

    2

--- js/service/upload.service.js
+++ js/service/upload.service.js
@@ -1,33 +1,25 @@
-function uploadImg() {
-  const imgDataUrl = gElCanvas.toDataURL("image/jpeg")// Gets the canvas content as an image format
-  function onSuccess(uploadedImgUrl) {
-    const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl)
-    console.log(encodedUploadedImgUrl)
 
-    window.open(`https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}`)
+function onShareCanvas() {
+  const imgDataUrl = gElCanvas.toDataURL("image/jpeg");
+  function onSuccess(uploadedImgUrl) {
+    const shareData = {
+      title: 'MDN',
+      text: 'Learn web development on MDN!',
+      url: uploadedImgUrl
+    }
+    navigator.share(shareData);
   }
   doUploadImg(imgDataUrl, onSuccess)
 }
 
 function doUploadImg(imgDataUrl, onSuccess) {
-  // Pack the image for delivery
   const formData = new FormData()
   formData.append('img', imgDataUrl)
-
-  // Send a post req with the image to the server
   const XHR = new XMLHttpRequest()
   XHR.onreadystatechange = () => {
-    // If the request is not done, we have no business here yet, so return
     if (XHR.readyState !== XMLHttpRequest.DONE) return
-    // if the response is not ok, show an error
     if (XHR.status !== 200) return console.error('Error uploading image')
     const { responseText: url } = XHR
-    // Same as:
-    // const url = XHR.responseText
-
-    // If the response is ok, call the onSuccess callback function, 
-    // that will create the link to facebook using the url we got
-    console.log('Got back live url:', url)
     onSuccess(url)
   }
   XHR.onerror = (req, ev) => {
@@ -36,8 +28,6 @@ function doUploadImg(imgDataUrl, onSuccess) {
   XHR.open('POST', '//ca-upload.com/here/upload.php')
   XHR.send(formData)
 }
-
-
 
 
 
